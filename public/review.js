303349b36a11acd7e48217a97ba679240470fef1
Mohammad Fathi <mohfabdou@gmail.com>
2024-11-17 00:04:54 +0200

    init

--- public/review.js
+++ public/review.js
@@ -7,7 +7,40 @@ console.log(fen)
 game.load_pgn(pgn);
 
 const board = Chessboard2('game_review_board', fen);
+const lastMove = setLastMove(pgn);
+const movesArr = setupMovesArr(pgn)
+console.log("Moves: ", movesArr)
+
 var currentPgn = pgn;
+var currentBoardMove = lastMove;
+
+
+function setupMovesArr(pgn){
+    var splitted = pgn.split(' ')
+    var output = []
+    for(let i = 0 ; i < splitted.length; i += 3){
+        let x = []
+        x[0] = splitted[i]
+        x[1] = splitted[i+1]
+        if(i + 2 < splitted.length)
+            x[2] = splitted[i+2]
+        output.push(x)
+    }
+
+    return output
+}
+
+
+function setLastMove(pgn){
+    let lastDot = pgn.lastIndexOf(".");
+    let lastSpc = lastDot;
+    for(;pgn[lastSpc] != ' '; lastSpc--){
+        ;
+    }
+
+    return lastSpc + 1;
+}
+
 
 $('.moveCard').on('click', function(){
     let selectedMove = $(this).attr('data-move');
@@ -38,4 +71,20 @@ $('.moveCard').on('click', function(){
     console.log(currentPgn)
     game.load_pgn(currentPgn)
     board.position(game.fen())
+})
+
+$('#lastPosition').on('click', function(){
+    currentBoardMove = lastMove;
+    game.load_pgn(pgn)
+    board.position(game.fen())
+})
+
+$('#firstPosition').on('click', function(){
+    currentBoardMove = pgn.indexOf("1.");
+    game.reset()
+    board.position(game.fen())
+})
+
+$('#nextMove').on('click', function(){
+    // for(let i = currentBoardMove)
 })
