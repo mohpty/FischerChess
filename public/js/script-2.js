0e9c318d452d3e020535c87213e911704fff28d7
Abdu-Kh <gedosh166@gmail.com>
2024-11-17 01:10:34 +0200

    Update index and play UI

---
+++ public/js/script-2.js
@@ -0,0 +1,12 @@
+$(document)(function() {             
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('#loginModal').click(function(){
+      $('#loginModal').modal('show');
+    });
+    $('#signupModal').click(function(){
+      $('#signupModal').modal('show');
+    });
+    
+});
+
